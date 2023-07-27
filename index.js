6b9f8dee3ce45bd87f255167875656e4bbed6595
melindamackenzie <mem167@pitt.edu>
2023-07-27 15:20:52 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+function findMatching(array, name) {
+    return array.filter (function (string) {
+        return string.toLowerCase() === name.toLowerCase()
+
+
+    })
+}
+
+
+
+
+function fuzzyMatch (array, string){
+    return array.filter (function (name) {
+        return name.toLowerCase().startsWith(string.toLowerCase()) 
+    })
+}
+
+function matchName(array,string){
+    return array.filter (function (drivers) {
+        return drivers.name === string})
+    }
